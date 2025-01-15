bb01a98095d5bf57b0f90203a14f74d56914a6db
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-15 16:37:27 +0100

    ADD input

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import GroupDetail from './pages/GroupDetail'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { GlobalContext } from './GlobalContext';
+import { useState } from 'react';
 
 function App() {
-
+  const [string, setString ] = useState('')
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/:id' element={<GroupDetail/>} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalContext.Provider value={{string, setString}}>
+     <BrowserRouter>
+       <Routes>
+         <Route path='/' element={<HomePage/>}/>
+         <Route path='/:id' element={<GroupDetail/>} />
+       </Routes>
+     </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
