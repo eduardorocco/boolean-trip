36ae29f6b8cf4cd4bb42563f4e257ca25f79ba6d
antoniorusso2 <russoantonio303@outlook.it>
2025-01-16 10:18:39 +0100

    fix group details

--- src/pages/GroupDetail.jsx
+++ src/pages/GroupDetail.jsx
@@ -9,7 +9,6 @@ import { useState } from 'react';
 import { Filter } from '../Filter.jsx';
 
 export default function GroupDetail() {
-<<<<<<< HEAD
   const { string } = useContext(GlobalContext);
   const { id } = useParams();
   const { persone } = viaggi[id];
@@ -53,45 +52,4 @@ export default function GroupDetail() {
       </Row>
     </Container>
   );
-=======
-    const { string } = useContext(GlobalContext)
-    const { id } = useParams()
-    const { persone } = viaggi[id]
-
-    const [personeReattive, setPersoneReattive] = useState(persone)
-
-    useEffect(() => {
-        setPersoneReattive(
-            persone.filter((persona) => persona.nome.toLowerCase().includes(string.toLowerCase()) || persona.cognome.toLowerCase().includes(string.toLowerCase()))
-        )
-    }, [string, persone])
-
-    return (
-        <Container className="mt-5">
-            <Link to="/" className="btn btn-primary mb-3">
-                Torna alla home
-            </Link>
-            <Filter />
-            <Row className="gap-2">
-                {personeReattive.map((persona, index) => (
-                    <Accordion key={index}>
-                        <Accordion.Item eventKey={index}>
-                            <Accordion.Header>{persona.nome} {persona.cognome}</Accordion.Header>
-                            <Accordion.Body>
-                                Codice fiscale: {persona.cod_fiscale}<br />
-                                Email: {persona.email}<br />
-                                Numero di telefono: {persona.numero_telefono}<br />
-                                Numero estero: {persona.numero_estero}<br />
-                                Contatto di emergenza: {persona.contatto_emergenza}
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-                ))}
-            </Row>
-        </Container>
-
-    )
->>>>>>> f88f0a81a7c262145dbddc74017b0c43219a7dd6
 }
-
-//push tesh
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,43 +1,41 @@
-import viaggi from "../data/data.js";
-import { Container, Row, Card, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import viaggi from '../data/data.js';
+import { Container, Row, Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { Filter } from '../Filter.jsx';
-import { GlobalContext } from "../GlobalContext.jsx"
-import { useContext } from "react"
-import { useEffect } from "react";
-import { useState } from "react";
+import { GlobalContext } from '../GlobalContext.jsx';
+import { useContext } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function HomePage() {
+  const { string } = useContext(GlobalContext);
+  const [viaggiFinali, setViaggiFinali] = useState(viaggi);
 
-    const {string} = useContext(GlobalContext)
-    const [viaggiFinali, setViaggiFinali] =useState(viaggi)
+  useEffect(() => {
+    setViaggiFinali(viaggi.filter((viaggio) => viaggio.luogo.toLowerCase().includes(string)));
+  }, [string]);
 
-    useEffect(()=>{
-        setViaggiFinali(
-            viaggi.filter((viaggio)=> viaggio.luogo.toLowerCase().includes(string))
-        )
-    },[string])
-
-    return (
-        <Container className="mt-5">
-            <Filter/>
-            <Row className="flex-column gap-2">
-                {viaggiFinali.map((viaggio, index) => (
-                    <Card key={index} style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Card.Title>{viaggio.luogo}</Card.Title>
-                            <Card.Text>
-                                Inizio: {viaggio.inizio}<br />
-                                Fine: {viaggio.fine}<br />
-                                Costo: {viaggio.costo}
-                            </Card.Text>
-                            <Link to={`/${index}`}>
-                                <Button>Dettagli</Button>
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                ))}
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="mt-5">
+      <Filter />
+      <Row className="flex-column gap-2">
+        {viaggiFinali.map((viaggio, index) => (
+          <Card key={index} style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>{viaggio.luogo}</Card.Title>
+              <Card.Text>
+                Inizio: {viaggio.inizio}
+                <br />
+                Fine: {viaggio.fine}
+                <br />
+              </Card.Text>
+              <Link to={`/${index}`}>
+                <Button>Dettagli</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </Row>
+    </Container>
+  );
 }
