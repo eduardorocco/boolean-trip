8b28acc51d3c8d7674c293a3237251e04497e537
antoniorusso2 <russoantonio303@outlook.it>
2025-01-16 10:06:37 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import GroupDetail from './pages/GroupDetail'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import GroupDetail from './pages/GroupDetail';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalContext } from './GlobalContext';
 import { useState } from 'react';
 
-
-
 function App() {
-  const [string, setString ] = useState('')
-
+  const [string, setString] = useState('');
 
   return (
-    <GlobalContext.Provider value={{string, setString}}>
-     <BrowserRouter>
-       <Routes>
-         <Route path='/' element={<HomePage/>}/>
-         <Route path='/:id' element={<GroupDetail/>} />
-       </Routes>
-     </BrowserRouter>
+    <GlobalContext.Provider value={{ string, setString }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<GroupDetail />} />
+        </Routes>
+      </BrowserRouter>
     </GlobalContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
